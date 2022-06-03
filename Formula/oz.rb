92c88df4dccc1842cf29fd7617ad6d03e9a02a82
Olivier Voortman <ov@taktik.com>
2022-06-03 16:31:03 +0200

    Updated Ozone client-cli

--- Formula/oz.rb
+++ Formula/oz.rb
@@ -1,10 +1,10 @@
 class Oz < Formula
-  @@git_version = "3.3.4589-g2c53f6596c"
+  @@git_version = "3.3.4590-g170493e3ad"
   desc "Ozone Command Line Interface"
   homepage "https://www.taktik.com/"
   url "https://maven.taktik.be/repository/releases/com/taktik/ozone/client-cli/#@@git_version/client-cli-#@@git_version.jar"
   version "#@@git_version"
-  sha256 "482e63ad59cb2d3db177401902e6cd71a7a472016689171dbf2108f547c5447e"
+  sha256 "f6871d7144c2d305637442563556ab0b98b3a8b9f603d67887dcb0167a7e994f"
 
   bottle :unneeded
 
